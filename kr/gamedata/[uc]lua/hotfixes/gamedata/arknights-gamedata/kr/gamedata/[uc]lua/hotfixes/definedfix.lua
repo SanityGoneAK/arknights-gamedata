770e9cb480d9b12681e921bea09d99d76efa86b9
Sanity;Gone <actions@users.noreply.github.com>
2026-02-10 01:37:10 +0000

    update(game-data): Updated Gamedata

---
+++ kr/gamedata/[uc]lua/hotfixes/gamedata/arknights-gamedata/kr/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -0,0 +1,14 @@
+
+local list = 
+{
+  
+  "HotFixes/TestStubHotfixer",
+  "HotFixes/Act1vHalfidleHotfixer",
+  "HotFixes/Mainline16ManagerHotfixer",
+  "HotFixes/BattleControllerHotfixer",
+  "HotFixes/ParamVariablePoolHotfixer",
+  "HotFixes/HomeThemeApNumbersViewHotfixer",
+  "HotFixes/MultiV3Hotfixer"
+};
+
+return list;
