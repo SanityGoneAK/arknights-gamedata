68d29fcd42092aae6d2d6266bf2c069dc15bb334
Sanity;Gone <actions@users.noreply.github.com>
2026-03-09 23:34:47 +0000

    update(game-data): Updated Gamedata

---
+++ cn/gamedata/[uc]lua/hotfixes/gamedata/arknights-gamedata/cn/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -0,0 +1,14 @@
+
+local list = 
+{
+  
+  "HotFixes/TestStubHotfixer",
+  "HotFixes/CGGalleryHotfixer",
+  "HotFixes/AutoChessUtilHotfixer",
+  "HotFixes/AdvancedSelectorWithEnemyOptionsHotfixer",
+  "HotFixes/PCHotfixer",
+  "HotFixes/AbilityEventCounterHotfixer",
+  "HotFixes/WangVisualStoneCtrlAbilityHotfixer"
+};
+
+return list;
