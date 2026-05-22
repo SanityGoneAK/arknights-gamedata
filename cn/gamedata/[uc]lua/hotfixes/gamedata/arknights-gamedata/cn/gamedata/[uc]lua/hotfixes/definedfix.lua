ca57e62377c3f5cf83d361f0e706983076f06340
Sanity;Gone <actions@users.noreply.github.com>
2026-05-22 09:37:19 +0000

    update(game-data): Updated Gamedata

--- cn/gamedata/[uc]lua/hotfixes/gamedata/arknights-gamedata/cn/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ cn/gamedata/[uc]lua/hotfixes/gamedata/arknights-gamedata/cn/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,10 +3,23 @@ local list =
 {
   
   "HotFixes/TestStubHotfixer",
-  "HotFixes/PCHotfixer",
-  "HotFixes/UICharTabGroupInAutochessHotfixer",
-  "HotFixes/EPControllerHotfixer",
-  "Hotfixes/AVGControllerHotfixer"
+  "HotFixes/SandboxV3BattleManagerHotfixer",
+  "HotFixes/VSandBoxFurnitureEntityHotfixer",
+  "HotFixes/BuildingScoreManagerHotfixer",
+  "HotFixes/SandboxV3BuildUtilsHotfixer",
+  "HotFixes/AdvancedSelectorForSandboxV3GatherHotfixer",
+  "HotFixes/FogCameraHotfixer",
+  "HotFixes/SandboxV3BattleUtilsHotfixer",
+  "HotFixes/SandboxV3DungeonHotfixer",
+  "HotFixes/EnemyDualHotfixer",
+  "HotFixes/NameCardSkinParticleScalerHotfixer",
+  "HotFixes/RoguelikeDeifyGameModeHotfixer",
+  "HotFixes/AVGControllerHotfixer",
+  "HotFixes/SandboxV3CatchedAnimalManagerHotfixer",
+  "HotFixes/SandboxV3BasementDetailStateHotfixer",
+  "HotFixes/ReturnConstructMaterialProcessorHolderHotfixer",
+  "HotFixes/SandboxV3BuildSaveManagerHotfixer",
+  "HotFixes/DialogSandboxV3BuildPluginHotfixer",
 };
 
 return list;
